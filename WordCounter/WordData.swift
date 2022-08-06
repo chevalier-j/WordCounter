af8a01b13510f8e70c7699c9f4b3cbacc916014c
gibert-j <adelgibert@gmail.com>
2022-08-06 21:50:51 +1200

    Sorted allWords to show most repeated words on top

--- WordCounter/WordData.swift
+++ WordCounter/WordData.swift
@@ -38,12 +38,16 @@ class WordData {
 //                // new array from dictionary keys. Removes all duplicate words
 //                allWords = Array(wordCounts.keys)
 
-                
                 // creates counted set from all the words: counts and removes duplicates
-                wordCounts = NSCountedSet(array: allWords)
+                //wordCounts = NSCountedSet(array: allWords)
                 // update allWords array with de-duplicated results from counted set
-                allWords = wordCounts.allObjects as! [String]
+                //allWords = wordCounts.allObjects as! [String]
                 
+                // sorting the array:
+                // reformating code above with shorthand. Using a closure to compare first word $0 with second $1. Return true if $0 should sorted before $1, depending on count
+                wordCounts = NSCountedSet(array: allWords)
+                let sorted = wordCounts.allObjects.sorted { wordCounts.count(for: $0) > wordCounts.count(for: $1) }
+                allWords = sorted as! [String]
                 
             }
         }
